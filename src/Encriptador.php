d97e9318f199c5c31fc74a447fb889b98eb661b6
cotoflux <guaupelu@hotmail.com>
2019-11-20 17:57:01 +0100

    seter created

--- src/Encriptador.php
+++ src/Encriptador.php
@@ -3,6 +3,24 @@ namespace Proyecto;
 
 class Encriptador
 {
+    public $inputTexto;
+    private $clavePrivada;
 
+    function strartEncription()
+    {
+
+    }
+    function keyGenerate()
+    {
+
+    }
+    function setKey($valor)
+    {
+        $this->clavePrivada = $value ;
+    }
+    function getKey()
+    {
+        return $this->clavePrivada;
+    }
 }
 
